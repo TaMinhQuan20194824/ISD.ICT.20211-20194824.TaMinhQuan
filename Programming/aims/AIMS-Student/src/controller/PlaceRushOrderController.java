598b4837074a8c0ba76044156b73b4d5f407134f
quanta123645 <pdcoc123@gmail.com>
2021-12-07 21:52:37 +0700

    Refactor PlaceRushOrderController according to checkstyle

--- Programming/aims/AIMS-Student/src/controller/PlaceRushOrderController.java
+++ Programming/aims/AIMS-Student/src/controller/PlaceRushOrderController.java
@@ -9,46 +9,56 @@ package controller;
  */
 public class PlaceRushOrderController extends BaseController {
 
-	/**
-	 * This method validate the arrival time of media.
-	 * 
-	 * @param earliestArrivalTime
-	 * @param latestArrivalTime
-	 * @return validation result: true or false
-	 */
-	public boolean validateArrivalTime(String earliestArrivalTime, String latestArrivalTime) {
-		// check input time are not null
-		if (earliestArrivalTime == null) return false;
-		if (latestArrivalTime == null) return false;
-		
-		// split input time 
-		int[] earliestArrivalTimeToken = new int[3];
-		int[] latestArrivalTimeToken = new int[3];
-		
-		int i = 0;
-		for(String x:earliestArrivalTime.split("-")) {
-			earliestArrivalTimeToken[i]= Integer.parseInt(x); 
-			i++;
-		}
-		
-		i = 0;
-		for(String x:latestArrivalTime.split("-")) {
-			latestArrivalTimeToken[i]= Integer.parseInt(x); 
-			i++;
-		}
-		
-		// check latest time > earliest time 
-		if (earliestArrivalTimeToken[2] < latestArrivalTimeToken[2]) {
-			return true;
-		} else if (earliestArrivalTimeToken[2] == latestArrivalTimeToken[2]) {
-			if (earliestArrivalTimeToken[1] < latestArrivalTimeToken[1]) {
-				return true;
-			} else if(earliestArrivalTimeToken[1] == latestArrivalTimeToken[1]) {
-				if (earliestArrivalTimeToken[0] < latestArrivalTimeToken[0]) {
-					return true;
-				} else return false;
-			} else return false;
-		} else return false;	
-	}
+  /**
+   * This method validate the arrival time of media.
+   * 
+   * @param earliestArrivalTime - the earliest time for medias to arrive
+   * @param latestArrivalTime - the latest time for medias to arrive
+   * @return validation result: true or false
+   */
+  public boolean validateArrivalTime(String earliestArrivalTime, String latestArrivalTime) {
+    // check input time are not null
+    if (earliestArrivalTime == null) { 
+      return false;
+    }
+    if (latestArrivalTime == null) {
+      return false;
+    }
+
+    // split input time
+    int[] earliestArrivalTimeToken = new int[3];
+    int[] latestArrivalTimeToken = new int[3];
+
+    int i = 0;
+    for (String x : earliestArrivalTime.split("-")) {
+      earliestArrivalTimeToken[i] = Integer.parseInt(x);
+      i++;
+    }
+
+    i = 0;
+    for (String x : latestArrivalTime.split("-")) {
+      latestArrivalTimeToken[i] = Integer.parseInt(x);
+      i++;
+    }
+
+    // check latest time > earliest time
+    if (earliestArrivalTimeToken[2] < latestArrivalTimeToken[2]) {
+      return true;
+    } else if (earliestArrivalTimeToken[2] == latestArrivalTimeToken[2]) {
+      if (earliestArrivalTimeToken[1] < latestArrivalTimeToken[1]) {
+        return true;
+      } else if (earliestArrivalTimeToken[1] == latestArrivalTimeToken[1]) {
+        if (earliestArrivalTimeToken[0] < latestArrivalTimeToken[0]) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
 	
 }
